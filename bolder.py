314485324089f0e6f4b378a49f4043934836fab0
P.S.S.ABISHEK <pssabishek18@gmail.com>
2020-03-05 15:39:40 +0530

    second commit

--- bolder.py
+++ bolder.py
@@ -1,33 +1,54 @@
+import tkinter as tk
+import os.path
 from docx import Document
 from docx.shared import Inches
-from tkinter import *
-import tkinter as tk
 
+def show_entry_fields():
+    if os.path.isfile('./InputFolder/'+e1.get()+'.docx'):
+        doc = Document('./InputFolder/'+e1.get()+'.docx')
+        doc1 = Document()
 
-def makeItBold (): 
-    doc = Document('./questions.docx')
-    doc1 = Document()
+        for i in doc.paragraphs:
+            if 'M:' in i.text:
+                p = doc1.add_paragraph()
+                p.add_run(i.text).bold = True
+            else:
+                p = doc1.add_paragraph()
+                p.add_run(i.text)
 
-    for i in doc.paragraphs:
-        if 'M:' in i.text:
-            p = doc1.add_paragraph()
-            p.add_run(i.text).bold = True
+        doc1.save('./OutputFolder/'+e2.get()+'.docx')
+        if os.path.isfile('./OutputFolder/'+e2.get()+'.docx'):
+            tk.Label(master, text= '                                                      ', fg='green', font=('helvetica', 12, 'bold')).grid(row=2)
+            tk.Label(master, text= 'File Created', fg='green', font=('helvetica', 12, 'bold')).grid(row=2)
         else:
-            p = doc1.add_paragraph()
-            p.add_run(i.text)
+            tk.Label(master, text= '                                           ', fg='green', font=('helvetica', 12, 'bold')).grid(row=2)
+            tk.Label(master, text= 'Error Occured', fg='green', font=('helvetica', 12, 'bold')).grid(row=2) 
+    else:
+        tk.Label(master, text= '                                                    ', fg='green', font=('helvetica', 12, 'bold')).grid(row=2)
+        tk.Label(master, text= 'Input file does not exist', fg='green', font=('helvetica', 12, 'bold')).grid(row=2)
 
-    doc1.save('./FinalDocx/bolded.docx')
+master = tk.Tk()
+tk.Label(master, 
+         text="Input File Name").grid(row=0)
+tk.Label(master, 
+         text="Output File Name").grid(row=1)
 
-root= tk.Tk()
+e1 = tk.Entry(master)
+e2 = tk.Entry(master)
 
-canvas1 = tk.Canvas(root, width = 300, height = 300)
-canvas1.pack()
+e1.grid(row=0, column=1)
+e2.grid(row=1, column=1)
 
-def hello ():  
-    label1 = tk.Label(root, text= 'Hello World!', fg='green', font=('helvetica', 12, 'bold'))
-    canvas1.create_window(150, 200, window=label1)
-       
-button1 = tk.Button(text='Click Me',command=hello, bg='brown',fg='white')
-canvas1.create_window(150, 150, window=button1)
+tk.Button(master, 
+          text='Quit', 
+          command=master.quit).grid(row=3,
+                                    column=0, 
+                                    sticky=tk.W, 
+                                    pady=4)
+tk.Button(master, 
+          text='Submit', command=show_entry_fields).grid(row=3, 
+                                                       column=1, 
+                                                       sticky=tk.W, 
+                                                       pady=4)
 
-root.mainloop()
+tk.mainloop()
---
+++ readme.md
@@ -0,0 +1,11 @@
+# Sentence Bolder
+
+A script that makes a sentence bold starting with 'M: '.
+
+## Installation
+
+Use the package manager [pip](https://pip.pypa.io/en/stable/) to install foobar.
+
+```bash
+pip install python-docx
+```
